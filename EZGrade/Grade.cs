a7b39ed5eb0771cb08fc6b08ff5669dafc6b3634
Chris Koenig <chris.koenig@microsoft.com>
2011-08-29 14:28:49 -0500

    first commit

---
+++ EZGrade/Grade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EZGrade
+{
+    public class Grade
+    {
+        public int NumberMissed { get; set; }
+
+        public int GradeValue { get; set; }
+
+        public Grade(int i, int p)
+        {
+            // TODO: Complete member initialization
+            NumberMissed = i;
+            GradeValue = p;
+        }
+    }
+}
---
+++ EZGrade/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace EZGrade
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            int problems;
+            if (!Int32.TryParse(NumberOfQuestionsTextbox.Text, out problems))
+            {
+                ShowErrorMessage("Please enter only numbers");
+                NumberOfQuestionsTextbox.Focus();
+                return;
+            }
+
+            var grades = new List<Grade>();
+            for (double i = 1; i <= problems; i++)
+            {
+                double result = (problems - i) / problems;
+                double grade = Math.Round(result * 100, 0);
+                grades.Add(new Grade((int)i, (int)grade));
+            }
+            ScoresListBox.ItemsSource = grades;
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK);
+        }
+    }
+}
