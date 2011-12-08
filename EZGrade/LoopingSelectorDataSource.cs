aa874b4625943dc8080d681a11bba1eba9a8151a
Chris Koenig <chris.koenig@microsoft.com>
2011-12-08 01:37:01 -0600

    changing the UX to see if I like it better

---
+++ EZGrade/LoopingSelectorDataSource.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls.Primitives;
+
+namespace EZGrade
+{
+    /// <summary>
+    /// Provides a data source for a <see cref="LoopingSelector"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LoopingSelectorDataSource<T> : ILoopingSelectorDataSource where T : class
+    {
+        private readonly IList<T> list;
+        private T selectedItem;
+        private bool allowInfiniteScrolling;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoopingSelectorDataSource&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="list">The list.</param>
+        public LoopingSelectorDataSource(IList<T> list)
+            : this(list, null, false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoopingSelectorDataSource&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="list">The list.</param>
+        /// <param name="selectedValue">The selected value.</param>
+        public LoopingSelectorDataSource(IList<T> list, T selectedValue, bool allowInfiniteScrolling)
+        {
+            this.list = list;
+            this.selectedItem = selectedValue ?? list[0];
+            this.allowInfiniteScrolling = allowInfiniteScrolling;
+        }
+
+        /// <summary>
+        /// Raised when the selection changes.
+        /// </summary>
+        public event EventHandler<SelectionChangedEventArgs> SelectionChanged;
+
+        /// <summary>
+        /// Get the next datum, relative to an existing datum.
+        /// </summary>
+        /// <param name="relativeTo">The datum the return value will be relative to.</param>
+        /// <returns>The next datum.</returns>
+        protected virtual T GetNext(T relativeTo)
+        {
+            return this.GetRelativeTo(relativeTo, 1);
+        }
+
+        /// <summary>
+        /// Get the previous datum, relative to an existing datum.
+        /// </summary>
+        /// <param name="relativeTo">The datum the return value will be relative to.</param>
+        /// <returns>The previous datum.</returns>
+        protected virtual T GetPrevious(T relativeTo)
+        {
+            return this.GetRelativeTo(relativeTo, -1);
+        }
+
+        /// <summary>
+        /// The selected item. Should never be null.
+        /// </summary>
+        public virtual T SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                if (value != selectedItem)
+                {
+                    if ((null == value) || (null == selectedItem) || (value != selectedItem))
+                    {
+                        object previousSelectedItem = selectedItem;
+                        selectedItem = value;
+                        EventHandler<SelectionChangedEventArgs> handler = SelectionChanged;
+                        if (null != handler)
+                        {
+                            handler(this, new SelectionChangedEventArgs(new object[] { previousSelectedItem }, new object[] { selectedItem }));
+                        }
+                    }
+                }
+            }
+        }
+
+        private T GetRelativeTo(T relativeItem, int delta)
+        {
+            int index = this.list.IndexOf(relativeItem) + delta;
+
+            if (!this.allowInfiniteScrolling && (index < 0 || index > (this.list.Count - 1)))
+            {
+                return null;
+            }
+
+            if (index < 0)
+            {
+                index = (this.list.Count - 1);
+            }
+            else if (index > (this.list.Count - 1))
+            {
+                index = 0;
+            }
+
+            return this.list[index];
+        }
+
+        /// <summary>
+        /// Get the next datum, relative to an existing datum.
+        /// </summary>
+        /// <param name="relativeTo">The datum the return value will be relative to.</param>
+        /// <returns>The next datum.</returns>
+        object ILoopingSelectorDataSource.GetNext(object relativeTo)
+        {
+            return this.GetNext((T)relativeTo);
+        }
+
+        /// <summary>
+        /// Get the previous datum, relative to an existing datum.
+        /// </summary>
+        /// <param name="relativeTo">The datum the return value will be relative to.</param>
+        /// <returns>The previous datum.</returns>
+        object ILoopingSelectorDataSource.GetPrevious(object relativeTo)
+        {
+            return this.GetPrevious((T)relativeTo);
+        }
+
+        /// <summary>
+        /// The selected item. Should never be null.
+        /// </summary>
+        /// <value></value>
+        object ILoopingSelectorDataSource.SelectedItem
+        {
+            get { return this.SelectedItem; }
+            set { this.SelectedItem = (T)value; }
+        }
+    }
+}
--- EZGrade/MainPage.xaml.cs
+++ EZGrade/MainPage.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Marketplace;
 
 namespace EZGrade
 {
@@ -19,23 +14,52 @@ namespace EZGrade
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void ShowDataButton_Click(object sender, RoutedEventArgs e)
-        {
-            Calculate();
-        }
-
-        private void Calculate()
-        {
-            int problems;
-            if (!Int32.TryParse(NumberOfQuestionsTextbox.Text, out problems))
+            Loaded += (sender, e) =>
             {
-                ShowErrorMessage("Please enter only numbers");
-                NumberOfQuestionsTextbox.Focus();
-                return;
-            }
+                var li = new LicenseInformation();
+                if (!li.IsTrial())
+                {
+                    MyAdControl.Visibility = Visibility.Collapsed;
+                }
+                var ints = new List<string>();
+                for (int i = 1; i < 150; i++)
+                {
+                    ints.Add(i.ToString());
+                }
+                var ds = new LoopingSelectorDataSource<string>(ints);
+                NumberOfQuestions.DataSource = ds;
+                var initialValue = RetrieveInitialValue();
+                ds.SelectedItem = initialValue.ToString();
+                Calculate(initialValue);
+            };
+        }
 
+        private int RetrieveInitialValue()
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            if (settings.Keys.Count > 0)
+            {
+                return Int32.Parse(settings["initial_value"].ToString());
+            }
+            else
+            {
+                settings["initial_value"] = 25;
+                settings.Save();
+                return 25;
+            }
+        }
+
+        private void SetInitialValue(int value)
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            settings["initial_value"] = value;
+            settings.Save();
+        }
+
+        private void Calculate(int problems)
+        {
+            SetInitialValue(problems);
+            NumberOfQuestionsTextBox.Text = problems.ToString();
             var grades = new List<Grade>();
             for (double i = 1; i <= problems; i++)
             {
@@ -46,9 +70,29 @@ namespace EZGrade
             ScoresListBox.ItemsSource = grades;
         }
 
-        private void ShowErrorMessage(string message)
+        private void SetQuestionsButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(message, "Error", MessageBoxButton.OK);
+            SelectNumberOfQuestionsPopup.IsOpen = true;
+        }
+
+        private void SelectNumberOfQuestionsPopup_Closed(object sender, EventArgs e)
+        {
+            var item = NumberOfQuestions.DataSource.SelectedItem.ToString();
+            int problems = Int32.Parse(item);
+            Calculate(problems);
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            if (SelectNumberOfQuestionsPopup.IsOpen)
+            {
+                SelectNumberOfQuestionsPopup.IsOpen = false;
+                e.Cancel = true;
+            }
+            else
+            {
+                base.OnBackKeyPress(e);
+            }
         }
     }
 }
